f9f2a3a8aef59edcda736381d37e2f2a2855d014
titicoco67899 <turuqilif@gmail.com>
2025-02-07 12:33:30 +0100

    Update mylibrery (1).h

--- mylibrery (1).h
+++ mylibrery (1).h
@@ -62,7 +62,7 @@ void initializeMatrix(int rows, int cols, int matrix[rows][cols], int value);
 void printMatrix(int rows, int cols, int matrix[rows][cols]);
 void inputMatrix(int rows, int cols, int matrix[rows][cols]);
 void addMatrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols], int result[rows][cols]);
-
-
-
+void subtractMatrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols],int result[rows][cols]);
+void multiplyMatrices(int rows1, int cols1, int mat1[rows1][cols1], int rows2, int cols2, int mat2[rows2][cols2], int result[rows1][cols2]);
+void scalarMultiplyMatrix(int rows, int cols, int matrix[rows][cols], int scalar);
 #endif
